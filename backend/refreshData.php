0dab957c06146c0c34b1c3afb9059dc49d48af15
sky <sky.ilyashenko@gmail.com>
2016-05-21 13:43:13 +0300

    random continets

--- backend/refreshData.php
+++ backend/refreshData.php
@@ -4,6 +4,8 @@
 //$param2 = $_POST["param2"];
 //$param3 = $_POST["param3"];
 
+$continents = array("евразия", "северная америка", "южная америка", "африка", "автралия", "атнарктида");
+
 $res = array();
 $count = 1000;
 for($i = 1; $i <= $count; $i++){
@@ -12,6 +14,7 @@ for($i = 1; $i <= $count; $i++){
 		"name" => "city $i",
 		"year" => rand(1500, 2016),
 		"population" => rand(100, 100000),
+		"continent" => $continents[rand(0, 4)],
 		"image" => "http://lorempixel.com/150/150/city/" . rand(1, 10)
 	);
 };
